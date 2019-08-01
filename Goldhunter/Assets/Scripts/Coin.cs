ff8a21bc5a66a300ce690d6f8bd8dfceda1f7364
Michael Bissinger <Michael.Bissinger@inb.de>
2019-08-01 18:03:52 +0200

    start and execute coroutine spin() to rotate gameobject in Coin.cs

--- Goldhunter/Assets/Scripts/Coin.cs
+++ Goldhunter/Assets/Scripts/Coin.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    [SerializeField]
+    private float rotateSpeed = 1.0f;  // rotates per second
+
     // Start is called before the first frame update
     void Start()
     {
+
+        StartCoroutine(Spin());
         
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator Spin()
     {
-        
+        while (true)
+        {
+
+            transform.Rotate(transform.up, 360 * rotateSpeed * Time.deltaTime);
+            yield return 0;
+
+        }
     }
+    
 }
