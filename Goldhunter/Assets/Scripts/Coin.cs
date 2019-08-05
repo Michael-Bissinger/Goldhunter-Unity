9a4f5586804d24e2a89322cd68128aec7cd5a704
Michael Bissinger <Michael.Bissinger@inb.de>
2019-08-05 18:46:04 +0200

    create fields and private vars for moving coins up and down

--- Goldhunter/Assets/Scripts/Coin.cs
+++ Goldhunter/Assets/Scripts/Coin.cs
@@ -7,9 +7,19 @@ public class Coin : MonoBehaviour
     [SerializeField]
     private float rotateSpeed = 1.0f;  // rotates per second
 
+    [SerializeField]
+    private float floatSpeed = 0.5f; // In cycles (up and down) per second
+
+    [SerializeField]
+    private float movementDistance = 0.5f; // The maximum distance the coin can move up and down
+
+    private float startingY;
+    private bool isMovingUp = true;
+
     // Start is called before the first frame update
     void Start()
     {
+        startingY = transform.position.y;
         transform.Rotate(transform.up, Random.Range(0f, 360f));
         StartCoroutine(Spin());
         
