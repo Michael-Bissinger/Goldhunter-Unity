08bf9272b39462801193b5a0899caff01d38cf5c
Michael Bissinger <Michael.Bissinger@inb.de>
2019-08-08 18:07:46 +0200

    add awake method to check if _instance is null

--- Goldhunter/Assets/Scripts/Singleton.cs
+++ Goldhunter/Assets/Scripts/Singleton.cs
@@ -29,4 +29,17 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
         }
     }
 
+    public virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
 }
