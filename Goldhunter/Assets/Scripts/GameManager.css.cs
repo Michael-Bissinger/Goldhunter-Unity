a06e0710a73f820bd5b4afce0c7077b5c865201d
Michael-Bissinger <Michael.Bissinger@inb.de>
2019-08-16 11:55:15 +0200

    inherit from Singleton and create variables

--- Goldhunter/Assets/Scripts/GameManager.css.cs
+++ Goldhunter/Assets/Scripts/GameManager.css.cs
@@ -2,8 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
+
+	private float _timeRemaining;
+
+	public float TimeRemaining
+	{
+		get { return TimeRemaining;}
+		set {_timeRemaining = value;}
+	}
+
+	private float maxTime = 5 * 60; // Seconds
+
     // Start is called before the first frame update
     void Start()
     {
